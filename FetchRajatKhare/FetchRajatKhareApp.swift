68b814e22bff9350412fecef4da6128b2a312bf4
Rajat Khare <rdkhare123@gmail.com>
2023-03-13 03:36:49 -0500

    Initial Commit

---
+++ FetchRajatKhare/FetchRajatKhareApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRajatKhareApp.swift
+//  FetchRajatKhare
+//
+//  Created by Rajat Khare on 3/13/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchRajatKhareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
